bb96a71b8b204be3fe3ceb17fdeb93f1b043dd81
Marc <marcbeltran.alum@insestatut.cat>
2026-03-12 16:26:48 +0100

    primer commit

---
+++ Proj_V3.js
@@ -0,0 +1,11 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+let menu = "";
+
+console.log("1. Paladí humà"),
+console.log("2. Mag elf"),
+console.log("3. Guerrer humà"),
+console.log("4. Arquer mitjà")
+
+
+menu = prompt("Elecció: ");
