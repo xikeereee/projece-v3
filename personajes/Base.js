9489dd58dcecc548deb0f0d209faa845f70a3120
Marc <marcbeltran.alum@insestatut.cat>
2026-03-25 19:45:28 +0100

    commit 7

--- personajes/Base.js
+++ personajes/Base.js
@@ -7,7 +7,19 @@ class Base {
 
     constructor(Vida, Poder, Velocitat) {
         this.Vida = Vida
+        this.VidaMax = VidaMax
         this.Poder = Poder
         this.Velocitat = Velocitat
+    }
+
+    modVida(quantitat) {
+        this.vida += quantitat;
+
+        if(this.vida < 0) {
+            this.vida = 0
+        }
+        if (this.vida > this.VidaMax) {
+            this.vida = this.VidaMax;
+        }
     }
 };
