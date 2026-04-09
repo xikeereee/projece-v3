0a1f55fccac215208886177012bd2abe8551a120
Iker Moreno del Río <ikermorenodelrio.alum@insestatut.cat>
2026-04-09 16:48:16 +0200

    C25 - Ejemplo TerminalUtils

---
+++ test.js
@@ -0,0 +1,12 @@
+const { TerminalUtils } = require("terminalutils");
+
+async function main() {
+    let missatge = "Bienvenido Jugador 1";
+    for(let letras of missatge){
+        TerminalUtils.write(letras)
+        await TerminalUtils.espera(100)
+    }
+
+}
+
+main()
